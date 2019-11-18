fd0fd255abf9752604ed09d44e38b1f7076c29ac
Christos <constantinides.ch@gmail.com>
2019-11-18 16:51:51 +0200

    Updates

---
+++ precache-manifest.2c118162bc24266db47062c266c7cec3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "645dc9fbd87c8ef51c6b28f2bda4a60b",
+    "url": "/Countries-API/index.html"
+  },
+  {
+    "revision": "d686c90e0740caad4a96",
+    "url": "/Countries-API/static/css/main.18ff160c.chunk.css"
+  },
+  {
+    "revision": "f169c0dddbef1fda33b6",
+    "url": "/Countries-API/static/js/2.c84082e0.chunk.js"
+  },
+  {
+    "revision": "d686c90e0740caad4a96",
+    "url": "/Countries-API/static/js/main.e3f54f0f.chunk.js"
+  },
+  {
+    "revision": "4eb5ac3bf48440028a87",
+    "url": "/Countries-API/static/js/runtime~main.52a4f1bd.js"
+  }
+]);
