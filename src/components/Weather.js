45ecf152367fb29168774a38ee5bce666e773581
Christos <constantinides.ch@gmail.com>
2019-11-20 16:15:12 +0200

    Changed API back to Weatherstack after obtaining https access

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -6,15 +6,13 @@ const Weather = ({ country }) => {
   //initial state for weather
   const [weather, setWeather] = useState("");
   const [displayWeather, setDisplayWeather] = useState(false);
-
   //using API with the specific country capital name for weather as per docs
   useEffect(() => {
-    const key = `68af2c5582e650e0dc1c65f6c3717d65`;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&appid=${key}&units=metric`;
-
+    const key = `4dd06535601b78b0aa3200a31a2a0f43`;
+    const url = `https://api.weatherstack.com/current?access_key=${key}&query=${country.capital}`;
     axios.get(url).then(response => {
-      // console.log("response.data: ", response.data);
-      setWeather(response.data); //updating state based off JSON values
+      console.log("response.data: ", response.data);
+      setWeather(response.data.current); //updating state based off JSON values
       setDisplayWeather(true); //updating state
     });
   }, [country.capital]); //we want to only fetch data when the component mounts -> the effect depends on the query so when changed, data request is fired
@@ -27,36 +25,44 @@ const Weather = ({ country }) => {
       ) : (
         <div>
           <h3>Weather in {country.capital}</h3>
+          <div className={countryStyles.iconContainer}>
+            <img
+              className={countryStyles.weatherIcon}
+              src={weather.weather_icons}
+              alt={weather.weather_descriptions}
+            />
+          </div>
+
           <p>
-            <span className={countryStyles.detail}>Current temperature:</span>{" "}
-            {weather.main.temp}c
+            Current time is{" "}
+            <span className={countryStyles.detail}>
+              {weather.observation_time}
+            </span>
           </p>
           <p>
-            <span className={countryStyles.detail}>Min temp:</span>{" "}
-            {weather.main.temp_min}c
+            <span className={countryStyles.detail}>Temperature:</span>{" "}
+            {weather.temperature}c
           </p>
           <p>
-            <span className={countryStyles.detail}>Max temp:</span>{" "}
-            {weather.main.temp_max}c
+            <span className={countryStyles.detail}>Feels like...</span>{" "}
+            {weather.feelslike}c
           </p>
           <p>
             {" "}
             <span className={countryStyles.detail}>Conditions: </span>
-            {weather.weather[0].description}
-          </p>
-
-          <p>
-            <span className={countryStyles.detail}>Wind: </span>{" "}
-            {weather.wind.speed} kph with a direction of {weather.wind.deg}{" "}
-            degrees
+            {weather.weather_descriptions}
           </p>
           <p>
             <span className={countryStyles.detail}>Cloud coverage:</span>{" "}
-            {weather.clouds.all}%
+            {weather.cloudcover}%
+          </p>
+          <p>
+            <span className={countryStyles.detail}>Wind: </span>{" "}
+            {weather.wind_speed} kph {weather.wind_dir}
           </p>
           <p>
             <span className={countryStyles.detail}>Humidity:</span>{" "}
-            {weather.main.humidity}%
+            {weather.humidity}%
           </p>
         </div>
       )}
